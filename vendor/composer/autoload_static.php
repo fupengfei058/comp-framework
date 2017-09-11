32e7bd83aaf9abfb8e805d4f26be40acccb61c82
fupengfei <fupengfei@yaochufa.com>
2017-09-11 14:37:27 +0800

    增加路由

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0f2b175f0e296482e8caffaf461b220e
 {
+    public static $prefixLengthsPsr4 = array (
+        'N' => 
+        array (
+            'NoahBuscher\\Macaw\\' => 18,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'NoahBuscher\\Macaw\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/noahbuscher/macaw',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0f2b175f0e296482e8caffaf461b220e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0f2b175f0e296482e8caffaf461b220e::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
