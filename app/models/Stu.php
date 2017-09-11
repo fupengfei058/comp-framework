2f948ccdf41fe50714fa2844f841156229a2ddb5
fupengfei <fupengfei@yaochufa.com>
2017-09-11 16:32:34 +0800

    ORM

--- app/models/Stu.php
+++ app/models/Stu.php
@@ -1,23 +1,22 @@
 <?php
 
-//TODO 数据库可配置化
-
-class Stu {
+class Stu extends Illuminate\Database\Eloquent\Model {
+    public $timestamps = false;
     public static function test()
     {
-        try {
-            $pdo = new PDO("mysql:host=192.168.235.130;dbname=test",'root','root');
-            $pdo->query('set names utf8');
-        } catch (PDOException $e) {
-            die($e.getMessage());
-        }
-        $sql = "select * from stu where id<10";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-        $array = array();
-        while ($arr = $stmt->fetchAll(PDO::FETCH_ASSOC)){
-            $array[] = $arr;
-        }
-        var_dump($array);
+//        try {
+//            $pdo = new PDO("mysql:host=192.168.235.130;dbname=test",'root','root');
+//            $pdo->query('set names utf8');
+//        } catch (PDOException $e) {
+//            die($e.getMessage());
+//        }
+//        $sql = "select * from stu where id<10";
+//        $stmt = $pdo->prepare($sql);
+//        $stmt->execute();
+//        $array = array();
+//        while ($arr = $stmt->fetchAll(PDO::FETCH_ASSOC)){
+//            $array[] = $arr;
+//        }
+//        var_dump($array);
     }
 }
---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+return [
+
+    'driver'    => 'mysql',
+
+    'host'      => '192.168.235.130',
+
+    'database'  => 'test',
+
+    'username'  => 'root',
+
+    'password'  => 'root',
+
+    'charset'   => 'utf8',
+
+    'collation' => 'utf8_general_ci',
+
+    'prefix'    => ''
+
+];
