d4f79aeb885a66669a379d7984d3d101a0822ca9
fupengfei <fupengfei@yaochufa.com>
2017-09-11 16:40:26 +0800

    ORM

--- app/models/Stu.php
+++ app/models/Stu.php
@@ -4,19 +4,5 @@ class Stu extends Illuminate\Database\Eloquent\Model {
     public $timestamps = false;
     public static function test()
     {
-//        try {
-//            $pdo = new PDO("mysql:host=192.168.235.130;dbname=test",'root','root');
-//            $pdo->query('set names utf8');
-//        } catch (PDOException $e) {
-//            die($e.getMessage());
-//        }
-//        $sql = "select * from stu where id<10";
-//        $stmt = $pdo->prepare($sql);
-//        $stmt->execute();
-//        $array = array();
-//        while ($arr = $stmt->fetchAll(PDO::FETCH_ASSOC)){
-//            $array[] = $arr;
-//        }
-//        var_dump($array);
     }
 }
